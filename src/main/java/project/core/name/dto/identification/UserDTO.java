615f399c760a65553470c6a1145993d07af97c2e
serghei <cep6iu@gmail.com>
2024-08-31 21:45:35 +0300

    User Role validation

--- src/main/java/project/core/name/dto/identification/UserDTO.java
+++ src/main/java/project/core/name/dto/identification/UserDTO.java
@@ -1,6 +1,7 @@
 package project.core.name.dto.identification;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import project.core.name.enums.indentification.UserRole;
@@ -11,8 +12,9 @@ public class UserDTO {
     @NotBlank(message = "The login mustn't be empty")
     @Length(min = 3, max = 20, message = "The login must be between 3 and 20 characters.")
     private String login;
-    @NotBlank(message = "The password mustn't be empty")
-    @Length(min = 3, max = 20,  message = "The password have to be between 3 and 20 characters ")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$", message = "The password must contain at least: " +
+            "one lower case letter, one uppercase letter, one digit, one special character " +
+            "and must be at least 8 characters long")
     private String password;
     private UserRole role;
     private PersonalDataDTO personalData;
--- src/main/java/project/core/name/utils/identification/UserValidator.java
+++ src/main/java/project/core/name/utils/identification/UserValidator.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import project.core.name.dto.identification.UserDTO;
+import project.core.name.enums.indentification.UserRole;
 import project.core.name.repositories.identification.UserRepository;
+
+import java.util.List;
 
 @Component
 public class UserValidator implements Validator {
@@ -24,6 +27,10 @@ public class UserValidator implements Validator {
 
         if (userRepository.existsByLogin(user.getLogin())) {
             errors.rejectValue("login", "login.exists", "This login is already used");
+        }
+
+        if (!List.of(UserRole.values()).contains(user.getRole())) {
+            errors.rejectValue("role", "role.invalid", "This role is not valid");
         }
 
      }
